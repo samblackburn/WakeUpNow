df6d9a28fd787748f1f179ae4cc6150a5b910d45
Sam Blackburn <sam.blackburn@red-gate.com>
2020-04-24 13:57:32 +0100

    Start RDP session

--- WakeUpNow.ps1
+++ WakeUpNow.ps1
@@ -1,4 +1,4 @@
-$machineName = "DEV-SAM2"
+$machineName = "DEV-SAM2.red-gate.com"
 
 & $PSScriptRoot\.paket\paket.bootstrapper.exe
 & $PSScriptRoot\.paket\paket.exe install
@@ -7,10 +7,16 @@ $machineName = "DEV-SAM2"
 $domainCred = Request-RedGateDomainCredential
 $password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($domainCred.Password))
 $user = $domainCred.UserName
-"Connecting with username $user"
+"Connecting with username $user.  This should be your Redgate domain username without the domain."
 
 & "C:\Program Files (x86)\CheckPoint\Endpoint Connect\trac.exe" connect -u $user -p $password
 if ($LastExitCode) { throw "VPN returned exit code $LastExitCode" }
 
+& start "https://wol.red-gate.com/WakeUp?Name=$machineName"
+$a = 1 
 
-& start "https://wol.red-gate.com/WakeUp?Name=$machineName.red-gate.com"
+do {
+    ping $machineName -n 1
+} while ($LastExitCode)
+
+& "$($env:windir)\system32\mstsc.exe" /v:"$machineName"
