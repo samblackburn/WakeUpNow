3f8c6fd0befac9dcd3fe26789840db626dfbb755
Sam Blackburn <sam.blackburn@red-gate.com>
2020-04-24 09:41:20 +0100

    Start the VPN

--- WakeUpNow.ps1
+++ WakeUpNow.ps1
@@ -1,1 +1,16 @@
+$machineName = "DEV-SAM2"
+
 & $PSScriptRoot\.paket\paket.bootstrapper.exe
+& $PSScriptRoot\.paket\paket.exe install
+
+. $PSScriptRoot\Credentials.ps1
+$domainCred = Request-RedGateDomainCredential
+$password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($domainCred.Password))
+$user = $domainCred.UserName
+"Connecting with username $user"
+
+& "C:\Program Files (x86)\CheckPoint\Endpoint Connect\trac.exe" connect -u $user -p $password
+if ($LastExitCode) { throw "VPN returned exit code $LastExitCode" }
+
+
+& start "https://wol.red-gate.com/WakeUp?Name=$machineName.red-gate.com"
