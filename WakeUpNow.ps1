e85c0c8700118c2ac52d5d79bf57c99cef3fd1b6
Sam Blackburn <sam.blackburn@red-gate.com>
2020-04-24 14:01:27 +0100

    Scary warnings

--- WakeUpNow.ps1
+++ WakeUpNow.ps1
@@ -4,10 +4,16 @@ $machineName = "DEV-SAM2.red-gate.com"
 & $PSScriptRoot\.paket\paket.exe install
 
 . $PSScriptRoot\Credentials.ps1
+
+Write-Host "Enter your RG Domain creds.  The username should be your Redgate domain username without the domain."
+Write-Warning "Note: The creds will be saved in the Windows Credential Store."
+Write-Warning "Don't use this script unless you trust the security of your PC."
+Write-Warning "If your home machine gets hacked, you should change your RG domain password immediately."
+
 $domainCred = Request-RedGateDomainCredential
 $password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($domainCred.Password))
 $user = $domainCred.UserName
-"Connecting with username $user.  This should be your Redgate domain username without the domain."
+Write-Host "Connecting with username $user."
 
 & "C:\Program Files (x86)\CheckPoint\Endpoint Connect\trac.exe" connect -u $user -p $password
 if ($LastExitCode) { throw "VPN returned exit code $LastExitCode" }
